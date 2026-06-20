2537a58baecece5b7babb6d16e53f016f1ef4337
MariaA2005 <79272007+MariaA2005@users.noreply.github.com>
2026-06-20 19:10:59 +0200

    Update plot3D.m

--- plot3D.m
+++ plot3D.m
@@ -1,15 +1,22 @@
-function [] = plot3D(r_sat_ideal, r_sat_j2)
+function [] = plot3D(r_sat_ideal, varargin)
 figure('Color', 'w');
 hold on;
 plot3(r_sat_ideal(:,1), r_sat_ideal(:,2), r_sat_ideal(:,3), 'b', 'LineWidth', 1.5, 'DisplayName', 'Ideal (Kepler)');
-plot3(r_sat_j2(:,1), r_sat_j2(:,2), r_sat_j2(:,3), 'r--', 'LineWidth', 1.2, 'DisplayName', 'Perturbada (J2)');
 
+colors = ["#D95319", "#EDB120", "#7E2F8E", "#77AC30", "#4DBEEE", "#A2142F"];
+numOrbits = length(varargin);
+for i = 1:numOrbits
+    r_perturbed = varargin{i};
+    color_idx = mod(i-1, length(colors)) + 1;
+    label_name = sprintf('Perturbed Orbit %d', i);
+    plot3(r_perturbed(:,1), r_perturbed(:,2), r_perturbed(:,3), '--','Color', colors{color_idx}, 'LineWidth', 1.2, 'DisplayName', label_name);
+end
 % Graficar la Tierra a escala (en km)
 [unitX, unitY, unitZ] = sphere(20);
 surf(unitX*6378, unitY*6378, unitZ*6378, 'EdgeColor', 'none', 'FaceAlpha', 0.1);
 
 axis equal; grid on; legend('Location', 'best');
-title('Comparación de Órbitas: Ideal vs Perturbación J_2 (24h)');
+title('Comparación de Órbitas: Ideal vs Perturbaciones');
 xlabel('X (km)'); ylabel('Y (km)'); zlabel('Z (km)');
 view(3); % Forzar vista 3D
 end
