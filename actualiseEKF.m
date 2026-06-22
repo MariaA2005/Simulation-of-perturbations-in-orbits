a09b3d86c1d09a2c9f25b4b76bc3bf655ac32a88
MariaA2005 <79272007+MariaA2005@users.noreply.github.com>
2026-06-22 15:32:12 +0200

    formato

--- actualiseEKF.m
+++ actualiseEKF.m
@@ -1,16 +1,16 @@
 function [x_est, P] = actualiseEKF(sensorData, H, P_pred, R_gps, x_pred, k)
-z_k = sensorData.gpsPos(k, :)'; 
+    z_k = sensorData.gpsPos(k, :)';
     
     if sensorData.gpsNewData(k)
         % Innovación
         y_k = z_k - H * x_pred;
-        
+    
         % s
         S = H * P_pred * H' + R_gps;
-        
+    
         % ganancia K
-        K = P_pred * H' / S ; 
-        
+        K = P_pred * H' / S ;
+    
         % Actualizar
         x_est = x_pred + K * y_k;
         P = (eye(6) - K * H) * P_pred;
@@ -18,3 +18,4 @@ z_k = sensorData.gpsPos(k, :)';
         x_est = x_pred;
         P = P_pred;
     end
+end
